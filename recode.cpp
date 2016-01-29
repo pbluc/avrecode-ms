277d5856c11b63f5bb77733258be96f58da20e71
Chris Lesniewski <ctl@mit.edu>
2016-01-28 18:36:25 -0800

    Refactor CABAC decoding hooks and improve compressed proto.
    Separate the CABAC hooks into their own object, so that coded block
    decoding is independent of its harness. In principle, this means that
    frames could be decoded in parallel; it also opens the path to having
    multiple different coded block handlers.
    
    Improve the compressed file format. Record skipped coded blocks.
    
    Clean up the surrogate marker system so that we don't have as many casts
    and we don't have to check the input data for edge cases.

--- recode.cpp
+++ recode.cpp
@@ -1,10 +1,13 @@
 #include <fstream>
 #include <iostream>
+#include <list>
+#include <map>
 #include <sstream>
 #include <typeinfo>
 
 extern "C" {
 #include "libavcodec/avcodec.h"
+#include "libavcodec/cabac.h"
 #include "libavcodec/coding_hooks.h"
 #include "libavformat/avformat.h"
 #include "libavformat/avio.h"
@@ -13,6 +16,9 @@ extern "C" {
 
 #include "recode.pb.h"
 
+
+// CABAC blocks smaller than this will be skipped.
+const int SURROGATE_MARKER_BYTES = 8;
 
 
 template <typename T>
@@ -40,31 +46,11 @@ struct defer {
 };
 
 
-template <typename T>
-struct hooks {
-  static int read_packet(void *opaque, uint8_t *buffer_out, int size) {
-    T* object = static_cast<T*>(opaque);
-    return object->read_packet(buffer_out, size);
-  }
-  static void init_cabac_decoder(void *opaque, CABACContext *c, const uint8_t *buf, int size) {
-    T* object = static_cast<T*>(opaque);
-    object->init_cabac_decoder(c, buf, size);
-  }
-  static AVCodecCodingHooks coding_hooks(T* object) {
-    return {
-      object,
-      init_cabac_decoder,
-    };
-  }
-};
-
-
 // Sets up a libavcodec decoder with I/O and decoding hooks.
 template <typename Driver>
 class decoder {
  public:
-  decoder(Driver *driver, const std::string& input_filename)
-    : driver(driver), coding_hooks(hooks<Driver>::coding_hooks(driver)) {
+  decoder(Driver *driver, const std::string& input_filename) : driver(driver) {
     const size_t avio_ctx_buffer_size = 1024*1024;
     uint8_t *avio_ctx_buffer = static_cast<uint8_t*>(av_malloc(avio_ctx_buffer_size));
 
@@ -74,13 +60,13 @@ class decoder {
         avio_ctx_buffer,                      // input buffer
         avio_ctx_buffer_size,                 // input buffer size
         false,                                // stream is not writable
-        driver,                               // first argument for read_packet()
-        &hooks<Driver>::read_packet,          // read callback
+        this,                                 // first argument for read_packet()
+        read_packet,                          // read callback
         nullptr,                              // write_packet()
         nullptr);                             // seek()
 
     if (avformat_open_input(&format_ctx, input_filename.c_str(), nullptr, nullptr) < 0) {
-      throw std::invalid_argument("Failed to open file: " + input_filename);
+      throw std::invalid_argument("Failed to initialie decoding context: " + input_filename);
     }
   }
   ~decoder() {
@@ -126,9 +112,22 @@ class decoder {
   }
 
  private:
+  // Hook stubs - wrap driver into opaque pointers.
+  static int read_packet(void *opaque, uint8_t *buffer_out, int size) {
+    decoder *self = static_cast<decoder*>(opaque);
+    return self->driver->read_packet(buffer_out, size);
+  }
+  static void* init_cabac_decoder(void *opaque, CABACContext *ctx, const uint8_t *buf, int size) {
+    decoder *self = static_cast<decoder*>(opaque);
+    auto *cabac_decoder = new typename Driver::cabac_decoder(self->driver, ctx, buf, size);
+    self->cabac_contexts[ctx].reset(cabac_decoder);
+    return cabac_decoder;
+  }
+
   Driver *driver;
-  AVCodecCodingHooks coding_hooks;
   AVFormatContext *format_ctx;
+  AVCodecCodingHooks coding_hooks = { this, { init_cabac_decoder } };
+  std::map<CABACContext*, std::unique_ptr<typename Driver::cabac_decoder>> cabac_contexts;
 };
 
 
@@ -152,50 +151,57 @@ class compressor {
     d.decode_video();
 
     // Flush the final block to the output and write to stdout.
-    //std::cerr << "final gap: " << original_size - last_bit_block_end << std::endl;
-    out.add_literal_blocks(&original_bytes[last_bit_block_end], original_size - last_bit_block_end);
+    out.add_block()->set_literal(
+        &original_bytes[prev_coded_block_end], original_size - prev_coded_block_end);
     out_stream << out.SerializeAsString();
   }
 
   int read_packet(uint8_t *buffer_out, int size) {
     size = std::min<int>(size, original_size - read_offset);
-
-    //av_log(NULL, AV_LOG_INFO, "read_packet offset:%d size:%d\n", read_offset, size);
-
     memcpy(buffer_out, &original_bytes[read_offset], size);
     read_offset += size;
     return size;
   }
 
-  void init_cabac_decoder(CABACContext *c, const uint8_t *buf, int size) {
-    if (size < 8) return;  // Not enough space for surrogate header and index.
+  class cabac_decoder {
+   public:
+    cabac_decoder(compressor *c, CABACContext *ctx, const uint8_t *buf, int size) {
+      ::ff_reset_cabac_decoder(ctx, buf, size);
+      out = c->find_next_coded_block_and_emit_literal(buf, size);
+      if (out == nullptr) out = &out_to_ignore;
+      out->set_cabac(buf, size);
+    }
 
+   private:
+    Recoded::Block *out;
+    Recoded::Block out_to_ignore;
+  };
+
+ private:
+  Recoded::Block* find_next_coded_block_and_emit_literal(const uint8_t *buf, int size) {
     uint8_t *found = static_cast<uint8_t*>( memmem(
-        &original_bytes[last_bit_block_end], read_offset-last_bit_block_end,
+        &original_bytes[prev_coded_block_end], read_offset - prev_coded_block_end,
         buf, size) );
-    if (found != nullptr) {
-      int gap = found - &original_bytes[last_bit_block_end];
-      out.add_literal_blocks(&original_bytes[last_bit_block_end], gap);
-      out.add_cabac_blocks(&original_bytes[last_bit_block_end + gap], size);
-      last_bit_block_end += gap + size;
-      //std::cerr << "compressing bit block after gap: " << gap << " size: " << size << std::endl;
+    if (found && size >= SURROGATE_MARKER_BYTES) {
+      int gap = found - &original_bytes[prev_coded_block_end];
+      out.add_block()->set_literal(&original_bytes[prev_coded_block_end], gap);
+      prev_coded_block_end += gap + size;
+      return out.add_block();  // Return a block for the recoder to fill.
     } else {
-      //std::cerr << "cabac block with escapes, size: " << size << std::endl;
-      if (*reinterpret_cast<const uint32_t*>(buf) == 0UL) {
-        throw std::runtime_error("Can't compress this file, CABAC block starts with 4 zeros (unlikely!).");
-      }
+      // Can't recode this block, probably because it was NAL-escaped. Place
+      // a skip marker in the block list.
+      out.add_block()->set_skip_coded(size);
+      return nullptr;  // Tell the recoder to ignore this block.
     }
   }
 
- private:
   std::string input_filename;
   std::ostream& out_stream;
 
   uint8_t *original_bytes = nullptr;
   size_t original_size = 0;
   int read_offset = 0;
-
-  int last_bit_block_end = 0;
+  int prev_coded_block_end = 0;
 
   Recoded out;
 };
@@ -224,78 +230,113 @@ class decompressor {
 
   int read_packet(uint8_t *buffer_out, int size) {
     uint8_t *p = buffer_out;
-    while (size > 0) {
-      if (read_index >= in.literal_blocks_size()) {
-        break;
-      }
-      const std::string& literal = in.literal_blocks(read_index);
-      if (read_offset_literal < literal.size()) {
-        int n = literal.copy(reinterpret_cast<char*>(p), size, read_offset_literal);
-        read_offset_literal += n;
-        p += n;
-        size -= n;
-      } else if (read_index < in.cabac_blocks_size() &&
-          read_offset_surrogate < in.cabac_blocks(read_index).size()) {
-        std::string surrogate = make_surrogate_block(read_index);
-        int n = surrogate.copy(reinterpret_cast<char*>(p), size, read_offset_surrogate);
-        read_offset_surrogate += n;
-        p += n;
-        size -= n;
-      } else {
-        out_stream << in.literal_blocks(read_index);
-        if (read_index < in.cabac_blocks_size()) {
-          out_stream << in.cabac_blocks(read_index);
+    while (size > 0 && read_index < in.block_size()) {
+      if (read_block.empty()) {
+        const Recoded::Block& block = in.block(read_index);
+        if (block.has_literal()) {
+          read_block = block.literal();
+        } else if (block.has_cabac()) {
+          read_block += make_surrogate_block(read_index, block.cabac().size());
+        } else if (block.has_skip_coded()) {
+          surrogate_queue.push_back(std::make_pair("", read_index));
+        } else {
+          throw std::runtime_error("Unknown input block type");
         }
+      }
+      if (read_offset < read_block.size()) {
+        int n = read_block.copy(reinterpret_cast<char*>(p), size, read_offset);
+        read_offset += n;
+        p += n;
+        size -= n;
+      }
+      if (read_offset >= read_block.size()) {
+        out_stream << in.block(read_index).literal();
+        out_stream << in.block(read_index).cabac();
+
+        read_block.clear();
+        read_offset = 0;
         read_index++;
-        read_offset_literal = read_offset_surrogate = 0;
       }
     }
     return p - buffer_out;
   }
 
-  void init_cabac_decoder(CABACContext *c, const uint8_t *buf, int size) {
-    int surrogate_index = recognize_surrogate_block(buf, size);
-    if (surrogate_index >= 0) {
-      std::cerr << "Substituting CABAC block of size: " << size << " from index: " << surrogate_index << " current read_index is: " << read_index << std::endl;
-      const std::string& cabac = in.cabac_blocks(surrogate_index);
-      cabac.copy(reinterpret_cast<char*>(const_cast<uint8_t*>(buf)), size);
+  class cabac_decoder {
+   public:
+    cabac_decoder(decompressor *d, CABACContext *ctx, const uint8_t *buf, int size) {
+      const Recoded::Block& block = d->recognize_surrogate_block(buf, size);
+      if (block.has_cabac()) {
+        block.cabac().copy(reinterpret_cast<char*>(const_cast<uint8_t*>(buf)), size);
+      }
+      ::ff_reset_cabac_decoder(ctx, buf, size);
     }
-  }
+  };
 
  private:
-  std::string make_surrogate_block(int index) {
-    const std::string& cabac = in.cabac_blocks(index);
-    if (cabac.size() < 8) throw std::runtime_error("Invalid surrogate block size.");
-    if (index >= surrogate_index_max) throw std::runtime_error("Can't decode, too many surrogate blocks in file. (Unlikely to happen but could be fixed with decoder changes.)");
-
-    uint32_t header[2] = { surrogate_marker, surrogate_index_base + index };
-    std::string surrogate(reinterpret_cast<char*>(&header[0]), sizeof(header));
-    surrogate.resize(cabac.size(), 'X');  // NAL-safe padding.
+  std::string make_surrogate_block(int index, int size) {
+    if (size < SURROGATE_MARKER_BYTES) {
+      throw std::runtime_error("Invalid surrogate block size: " + std::to_string(size));
+    }
+    std::string surrogate = next_surrogate_marker();
+    surrogate_queue.push_back(std::make_pair(surrogate, index));
+    surrogate.resize(size, 'X');  // NAL-encoding-safe padding.
     return surrogate;
   }
-  
-  int recognize_surrogate_block(const uint8_t* buf, int size) {
-    if (size < 8) return -1;
-    const uint32_t *header = reinterpret_cast<const uint32_t*>(buf);
-    if (header[0] != surrogate_marker) return -1;
 
-    int index = header[1] - surrogate_index_base;
-    if (index >= in.cabac_blocks_size()) throw std::runtime_error("Invalid surrogate block index.");
-    const std::string& cabac = in.cabac_blocks(index);
-    if (cabac.size() != size) throw std::runtime_error("Invalid surrogate block size.");
-    return index;
+  // Return a unique 8-byte string containing no zero bytes (NAL-encoding-safe).
+  std::string next_surrogate_marker() {
+    if (surrogate_marker.empty()) {
+      surrogate_marker.resize(SURROGATE_MARKER_BYTES, '\x01');
+    }
+    for (int i = 0; i < surrogate_marker.size(); i++) {
+      if (surrogate_marker[i] != '\xFF') {
+        surrogate_marker[i]++;
+        return surrogate_marker;
+      } else {
+        // Reset to 1 and carry.
+        surrogate_marker[i] = '\x01';
+      }
+    }
+    throw std::runtime_error("More than 255^8 (~2^64) surrogate blocks (very unlikely).");
+  }
+  
+  const Recoded::Block& recognize_surrogate_block(const uint8_t* buf, int size) {
+    if (surrogate_queue.empty()) {
+      throw std::runtime_error("Coded block expected, but not recorded in the compressed data.");
+    }
+    const std::string& expected_surrogate_marker = surrogate_queue.front().first;
+    int index = surrogate_queue.front().second;
+    const Recoded::Block& block = in.block(index);
+    surrogate_queue.pop_front();
+
+    if (expected_surrogate_marker.empty()) {
+      if (!block.has_skip_coded()) throw std::runtime_error("Corrupt surrogate_queue");
+      if (block.skip_coded() != size) throw std::runtime_error("Invalid skip_coded block size");
+      return block;
+    }
+
+    if (block.cabac().size() != size) {
+      throw std::runtime_error("Invalid surrogate block size: expected " +
+          std::to_string(block.cabac().size()) + ", got " + std::to_string(size));
+    }
+    std::string buf_header(reinterpret_cast<const char*>(buf), expected_surrogate_marker.size());
+    if (expected_surrogate_marker != buf_header) {
+      throw std::runtime_error("Invalid surrogate marker in coded block.");
+    }
+    return block;
   }
 
   std::string input_filename;
   std::ostream& out_stream;
 
   Recoded in;
-  int read_index = 0, read_offset_literal = 0, read_offset_surrogate = 0;
+  int read_index = 0, read_offset = 0;
+  std::string read_block;
 
-  // Chosen for NAL-safe encoding: no sequences of '\x00\x00\x0[0123]'.
-  const uint32_t surrogate_marker = 0x12345678;
-  const uint32_t surrogate_index_base = 0x01030101;
-  const uint32_t surrogate_index_max = 0x02000000 - surrogate_index_base;
+  // List of (surrogate marker, block index) pairs. Blocks with skip_coded have marker=="".
+  std::list<std::pair<std::string, int>> surrogate_queue;
+  // Marker for the last surrogate block.
+  std::string surrogate_marker;
 };
 
 
@@ -312,7 +353,18 @@ int roundtrip(const std::string& input_filename, std::ostream* out) {
       (*out) << compressed.str();
     }
     double ratio = compressed.str().size() * 1.0 / original.str().size();
-    std::cout << "Compress-decompress roundtrip succeeded, ratio = " << ratio*100. << "%." << std::endl;
+
+    Recoded compressed_proto;
+    compressed_proto.ParseFromString(compressed.str());
+    int proto_block_bytes = 0;
+    for (const auto& block : compressed_proto.block()) {
+      proto_block_bytes += block.literal().size() + block.cabac().size();
+    }
+    double proto_overhead = (compressed.str().size() - proto_block_bytes) * 1.0 / compressed.str().size();
+
+    std::cout << "Compress-decompress roundtrip succeeded:" << std::endl;
+    std::cout << " compression ratio: " << ratio*100. << "%" << std::endl;
+    std::cout << " protobuf overhead: " << proto_overhead*100. << "%" << std::endl;
     return 0;
   } else {
     std::cerr << "Compress-decompress roundtrip failed." << std::endl;
